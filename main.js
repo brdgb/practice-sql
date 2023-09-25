63dbd016808dd7fe5d1d567413d61cd0812e89a6
brdgb <bridgebhot@gmail.com>
2023-09-25 21:30:44 +0900

    delete unnecessary code and fix format

--- main.js
+++ main.js
@@ -3,25 +3,28 @@ const express = require("express");
 const ejs = require("ejs");
 const app = express();
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 const prisma = require("@prisma/client");
 const client = new prisma.PrismaClient();
 
-app.get("/", async(request, response) => {
-    const posts = await (await client.Post.findMany()).map((post) => ({user: post.user, content: post.content}));
-    const template = fs.readFileSync("template.ejs", "utf-8");
-    const html = ejs.render(template, {
-        posts: posts,
-    });
-    response.send(html);
-})
-
-app.post("/send",async (request, response)=>{
-    await client.Post.create({data:{user: request.body.user, content: request.body.content}});
-    response.send("送信済み");
+app.get("/", async (request, response) => {
+  const posts = (await client.Post.findMany()).map((post) => ({
+    user: post.user,
+    content: post.content,
+  }));
+  const template = fs.readFileSync("template.ejs", "utf-8");
+  const html = ejs.render(template, {
+    posts: posts,
+  });
+  response.send(html);
 });
 
-
+app.post("/send", async (request, response) => {
+  await client.Post.create({
+    data: { user: request.body.user, content: request.body.content },
+  });
+  response.send("送信済み");
+});
 
 app.listen(3000);
